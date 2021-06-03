17cffaa44775d9def24cb078306d6de5f20d0a6b
אלישבע מנס <212127567@mbydomain>
2021-06-03 18:49:06 +0300

    potLesson

--- src/components/newClassRoom.js
+++ src/components/newClassRoom.js
@@ -1,49 +1,72 @@
-import React ,{useState}from 'react';
+import React, { useState } from 'react';
 import '../newClassRoom.css';
+import {newLessonToServer} from '../services/newClassRoom'
 
 const NewClassRoom = (props) => {
     const [numLesson, setNumLesson] = useState('');
     const [lessonName, setLessonName] = useState('');
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
+    const [file, setFile] = useState('');
+    const [date, setDate] = useState('');
+    const [notes, setNotes] = useState('');
+    const [time, setTime] = useState('');
 
 
+    const postLesson=async(numLesson, lessonName,file,date,notes,time)=>
+    {
+            let res = '';
+            res = await newLessonToServer(numLesson, lessonName,file,date,notes,time);
+            console.log(res);
+            alert("lesson send to server👍👍👍")
+        
+    }
     return (<div>
         <img className="classRoomImg" src={"/images/newClassRoom.PNG"} />
         {/* <select className="test" value={1,2,3} > מספר שיעור   </select> */}
-        <br/>
-        <input className="numLesson" type="number" min="0" max="24"   
-         value={numLesson} onChange={(e) =>{ 
-            console.log(e.target.value)
-            setNumLesson(e.target.value)}}/>
-        <br/>
-        <input type="text" 
-         value={lessonName} onChange={(e) =>{ 
-            console.log(e.target.value)
-            setLessonName(e.target.value)}}/>
-        <br/>
+        <br />
+        <input className="numLesson" type="number" min="0" max="24"
+            value={numLesson}
+            onChange={(e) => {
+                console.log(e.target.value)
+                setNumLesson(e.target.value)
+            }} />
+        <br />
+        <input type="text"
+            value={lessonName} onChange={(e) => {
+                console.log(e.target.value)
+                setLessonName(e.target.value)
+            }} />
+        <br />
         לבחירת קובץ:
-       <input type="file" title="בחר קובץ" /> <br />
-        {/* <input type="file" name="file1" >העלאת חומר לימוד</input> */}      
-        <br/>
-        <input className="dateLesson" type="date" /><br />
-        <textarea rows="8" cols="40" dir="rtl">
+        <input type="file" title="בחר קובץ"
+            value={file} onChange={(e) => {
+                console.log(e.target.value)
+                setFile(e.target.value)
+            }}
+        /> <br />
+        {/* <input type="file" name="file1" >העלאת חומר לימוד</input> */}
+        <br />
+        <input className="dateLesson" type="date"
+            value={date} onChange={(e) => {
+                console.log(e.target.value)
+                setDate(e.target.value)
+            }}
+        /><br />
+        <textarea rows="8" cols="40" dir="rtl"
+            value={notes} onChange={(e) => {
+                console.log(e.target.value)
+                setNotes(e.target.value)
+            }}>
             הכנס הערה
         </textarea><br />
-        <input type="time"/>
+        <input type="time"
+            value={time} onChange={(e) => {
+                console.log(e.target.value)
+                setTime(e.target.value)
+            }} />
+        <button className="button" onClick={() => postLesson(numLesson, lessonName,file,date,notes,time)}>  התחברות   </button>
+    </div>
 
+    );
+}
 
-                            {/* <img className="test" src={"/images/test.png"} />
-         <img className="hw" src={"/images/hw.png"} />
-         <img className="plessons" src={"/images/plessons.png"} /> */}
-                            {/* <button className="previouslessons" >  שיעורים קודמים   </button>
-    
-         <button className="test" > צפייה במבחנים  </button>
-         <button className="hw" > תרגילים שהוגשו   </button>
-         <button className="addendents" >  צפייה בנוכחות   </button> */}
-         </div>
-       
-        );
-    }
-    
-    export default NewClassRoom;
+export default NewClassRoom;
