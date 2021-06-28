38376407246b6a6c0a75f0e5de30063a680380fa
eshrem <eshrem@baybridgedigital.com>
2021-06-28 13:06:09 +0300

    popup

--- src/components/newTravel.js
+++ src/components/newTravel.js
@@ -1,36 +1,47 @@
- import React, { useState } from 'react';
- import { useHistory } from "react-router-dom";
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
 import Header from './header';
 
-const NewTravel=()=>
-{
-    const history = useHistory();
-        const passenger =()=>{
-           history.replace("/driverAndPassenger");     
-        }
-    
-        const driver =()=>{
-            history.replace("/driverAndPassenger");
-               
-         }
+import DriverAndPassenger from './driverAndPassenger';
+
+const NewTravel = () => {
+
+  const [showModel, setShowModel] = useState(false);
+  const [isDriver, setIsDriver] = useState(false);
+
+  const history = useHistory();
+
+  const passenger = () => {
+    setShowModel(true);
+    setIsDriver(false);
+  }
+
+  const driver = () => {
+    setShowModel(true);
+    setIsDriver(true);
+  }
 
 
 
 
 
-    return(<div>
-                  <Header/>
+  return (<div>
+    <Header />
 
+    <button onClick={() => driver()}> אני נהג   </button>
 
-      <button  onClick={() => driver()}> אני נהג   </button>
+    <button onClick={() => passenger()}>  אני נוסע   </button>
 
-      <button  onClick={() => passenger()}>  אני נוסע   </button>
+    <DriverAndPassenger
+      show={showModel}
+      onHide={(e) => setShowModel(false)}
+      isDriver={isDriver}
 
+    />
 
+  </div>
 
-    </div>
-
-    )
+  )
 
 }
 
