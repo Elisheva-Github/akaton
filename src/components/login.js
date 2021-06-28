48a640ea65696d471a23b63aa64c3eb194705bd0
MaliRose <mali41020@gmail.com>
2021-06-28 17:29:16 +0300

    changes

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from './header';
- import homeBG from '../images/homepage.jpg';
+import homeBG from '../images/homepage.png';
 
 import './homePage.css';
 // import { loginToServer } from '../services/login';
 const Login = (props) => {
 
 
-  return (<div class="bg-img" style={{backgroundImage: `url(${homeBG})`}} >
+  return (<div class="bg-img" style={{ backgroundImage: `url(${homeBG})` }} >
     <Header />
   </div>
   );
