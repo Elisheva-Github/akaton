745efdaf348cc9339da60e76d43d678d6ece81c9
eshrem <eshrem@baybridgedigital.com>
2021-06-28 19:52:24 +0300

    cc

--- src/components/newTravel.js
+++ src/components/newTravel.js
@@ -1,53 +1,3 @@
-// import React, { useState } from 'react';
-// import { useHistory } from "react-router-dom";
-// import Header from './header';
-
-// import DriverAndPassenger from './driverAndPassenger';
-
-// const NewTravel = () => {
-
-//   const [showModel, setShowModel] = useState(false);
-//   const [isDriver, setIsDriver] = useState(false);
-
-//   const history = useHistory();
-
-//   const passenger = () => {
-//     setShowModel(true);
-//     setIsDriver(false);
-//   }
-
-//   const driver = () => {
-//     setShowModel(true);
-//     setIsDriver(true);
-//   }
-
-
-
-
-
-//   return (<div>
-//     <Header />
-
-//     <button onClick={() => driver()}> אני נהג   </button>
-
-//     <button onClick={() => passenger()}>  אני נוסע   </button>
-
-//     <DriverAndPassenger
-//       show={showModel}
-//       onHide={(e) => setShowModel(false)}
-//       isDriver={isDriver}
-
-//     />
-
-//   </div>
-
-//   )
-
-// }
-
-
-// export default NewTravel;
-
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import Header from './header';
@@ -72,10 +22,6 @@ const NewTravel = () => {
     setShowModel(true);
     setIsDriver(true);
   }
-
-
-
-
 
   return (<div class="bg-img" style={{ backgroundImage: `url(${create})` }}>
     <Header />
