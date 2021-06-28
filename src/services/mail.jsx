33242c194bbe6bd64a01156ddce751128273cee2
MaliRose <mali41020@gmail.com>
2021-06-28 15:50:11 +0300

    mail

---
+++ src/services/mail.jsx
@@ -0,0 +1,14 @@
+export const mailToServer = (email) => {
+    fetch('http://localhost:3000/mail', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+
+            email,
+
+        })
+    });
+}
